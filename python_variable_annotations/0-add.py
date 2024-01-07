54b4dd733e51822d92a25b9866c64eb519bfd762
Salomon Chambi <salomonchambi@gmail.com>
2024-01-07 09:00:00 -0500

    feat: add annotations to add function

---
+++ python_variable_annotations/0-add.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""Module using type hints"""
+
+
+def add(a: float, b: float) -> float:
+    """
+    This function sums two floats
+
+    Parameters:
+        a: The first float
+        b: The second float
+
+    Returns:
+        Sum of two floats
+
+    """
+
+    return (a + b)
