6a15ced41a9a1b006ce0856f22d86a0e4836e172
Salomon Chambi <salomonchambi@gmail.com>
2024-01-07 09:00:00 -0500

    feat: add annotations to concat function

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""Handle of string with annotations"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    Parameters:
+        str1: The first string parameter
+        str1: The second string parameter
+
+    Return:
+        Concatenated string
+    """
+
+    return (str1 + str2)
