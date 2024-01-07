64cb7214466bb218ca25515beff9d566a123aa07
Salomon Chambi <salomonchambi@gmail.com>
2024-01-07 09:00:00 -0500

    feat: add annotations to floor function

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""Truncate float to integer"""
+
+
+def floor(n: float) -> int:
+    """
+    Parameters:
+        n: float number
+
+    Return:
+        Float number truncated to floor
+    """
+
+    return int(n)
