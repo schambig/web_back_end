4d805965fe628c56e8c5fe01e08bbdc94a77b99c
Salomon Chambi <salomonchambi@gmail.com>
2024-01-09 09:00:00 -0500

    fix: correct indentation on docstring block

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -6,12 +6,12 @@ from typing import Union, Tuple
 
 def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """
-        Args:
-            k: String
-            v: Union: Can be int or float
+    Parameters:
+        k: String
+        v: Union: Can be int or float
 
-        Return:
-            Tuple with string and int or float
+    Return:
+        Tuple with string and int or float
     """
 
     # cncat: Tuple(str, Union[int, float])  # Call expression not allowed in type expression
