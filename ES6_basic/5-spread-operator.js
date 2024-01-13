1b7e39986ed609cc33fadb6ab20c4ace01792662
Salomon Chambi <salomonchambi@gmail.com>
2024-01-13 09:00:00 -0500

    feat: use spread syntax to concatenate

--- ES6_basic/5-spread-operator.js
+++ ES6_basic/5-spread-operator.js
@@ -1,3 +1,11 @@
 export default function concatArrays(array1, array2, string) {
+  // Next conditionals are optional
+  if (!Array.isArray(array1) || !Array.isArray(array2)) {
+    throw new Error('Both parameters array1 and array2 must be arrays.');
+  }
+
+  if (typeof string !== 'string') {
+    throw new Error('The third parameter string must be a string.');
+  }
   return [...array1, ...array2, ...string];
 }
