bef9a4afddc49954037cb0f00ad4e384d92399b1
Salomon Chambi <salomonchambi@gmail.com>
2024-02-25 09:00:00 -0500

    feat: return an array of objects

--- ES6_data_manipulation/0-get_list_students.js
+++ ES6_data_manipulation/0-get_list_students.js
@@ -17,3 +17,15 @@ export default function getListStudents() {
     },
   ];
 }
+
+/* const getListStudents = () => {
+  const students = [
+    { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+    { id: 2, firstName: 'James', location: 'Columbia' },
+    { id: 5, firstName: 'Serena', location: 'San Francisco' },
+  ];
+
+  return students;
+};
+
+export default getListStudents; */
