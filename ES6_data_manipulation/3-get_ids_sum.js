a01dc4378423e224b26e636e69ed939d49637ce7
Salomon Chambi <salomonchambi@gmail.com>
2024-02-27 09:00:00 -0500

    feat: return the sum of all student ids

--- ES6_data_manipulation/3-get_ids_sum.js
+++ ES6_data_manipulation/3-get_ids_sum.js
@@ -3,3 +3,16 @@ export default function getStudentIdsSum(students) {
   // Accumulate the sum of student IDs starting from an initial value of 0
   return students.reduce((acumulator, student) => acumulator + student.id, 0);
 }
+
+/* const getStudentIdsSum = (students) => {
+  // Use the map() method to create a new array containing only the IDs of the students
+  const all = students
+    .map((student) => student.id)
+    // Use the reduce() method to calculate the sum of all student IDs
+    .reduce((acumulator, currentValue) => acumulator + currentValue);
+
+  // Return the total sum of student IDs
+  return all;
+};
+
+export default getStudentIdsSum; */
