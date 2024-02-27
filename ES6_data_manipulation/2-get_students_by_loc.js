e2ce7371b5755f5feb0849e1402338c713d6134a
Salomon Chambi <salomonchambi@gmail.com>
2024-02-27 09:00:00 -0500

    feat: return an array of objects who are located in a specific citys

--- ES6_data_manipulation/2-get_students_by_loc.js
+++ ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,6 +1,4 @@
 export default function getStudentsByLocation(students, city) {
-  // Use the filter method on the students array to create a new array containing only
-  // the student objects whose location matches the provided city
   return students.filter((student) => student.location === city);
 }
 
