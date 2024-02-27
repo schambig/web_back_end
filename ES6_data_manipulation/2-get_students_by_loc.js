7751ece9cc2a8faca596db9f62610f35399a486b
Salomon Chambi <salomonchambi@gmail.com>
2024-02-27 09:00:00 -0500

    feat: return an array of objects who are located in a specific citys

--- ES6_data_manipulation/2-get_students_by_loc.js
+++ ES6_data_manipulation/2-get_students_by_loc.js
@@ -3,3 +3,13 @@ export default function getStudentsByLocation(students, city) {
   // the student objects whose location matches the provided city
   return students.filter((student) => student.location === city);
 }
+
+/* const getStudentsByLocation = (students, city) => {
+  const studentsLocation = students.filter(
+    (student) => student.location === city,
+  );
+
+  return studentsLocation;
+};
+
+export default getStudentsByLocation; */
