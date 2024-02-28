2facfe22a3da80b698cd4ed387328e224306432b
Salomon Chambi <salomonchambi@gmail.com>
2024-02-28 09:00:00 -0500

    feat: return an array of students for a specific city with new grades

--- ES6_data_manipulation/4-update_grade_by_city.js
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -17,3 +17,32 @@ export default function updateStudentGradeByCity(students, city, newGrades) {
       return { ...student, grade: newGrade };
     });
 }
+
+/* const updateStudentGradeByCity = (students, city, newGrades) => {
+  // Check if the inputs are arrays, if not, return an empty array
+  if (!Array.isArray(students) || !Array.isArray(newGrades)) {
+    return [];
+  }
+  // Filter the students array to get only the students located in the specified city
+  // Then, map over the filtered students to update their grades based on the provided newGrades
+  const studentByCity = students
+    .filter((student) => student.location === city)
+    .map((student) => {
+      // Filter the newGrades array to get the grades of the current student
+      const grades = newGrades.filter((note) => student.id === note.studentId);
+      // Initialize a variable to hold the grade, defaulting to 'N/A'
+      let grade = 'N/A';
+
+      // If there is a grade found for the student, update the grade variable
+      if (grades[0]) {
+        grade = grades[0].grade;
+      }
+
+      // Return a new object representing the student with the updated grade
+      return { ...student, grade };
+    });
+
+  return studentByCity;
+};
+
+export default updateStudentGradeByCity; */
