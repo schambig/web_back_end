0ac9411dc9f2c362266bea4d06f408c90d7bd4cc
Salomon Chambi <salomonchambi@gmail.com>
2024-03-03 09:00:00 -0500

    feat: return boolean is all elems in array exist within the set

--- ES6_data_manipulation/7-has_array_values.js
+++ ES6_data_manipulation/7-has_array_values.js
@@ -6,3 +6,20 @@ The callback checks whether the Set contains the current value from the array
 */
   return array.every((value) => set.has(value));
 }
+
+/* const hasValuesFromArray = (set, array) => {
+  // Iterating over each item in the array using a for...of loop
+  for (const item of array) {
+    // Checking if the Set does not contain the current item
+    if (!set.has(item)) {
+      // If the Set does not contain the current item, returning false immediately
+      // as it means not all values in the array are present in the Set
+      return false;
+    }
+  }
+  // If the loop completes without encountering any missing values, returning true
+  // indicating that all values in the array are present in the Set
+  return true;
+};
+
+export default hasValuesFromArray; */
