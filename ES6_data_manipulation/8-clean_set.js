10bdf303ceea82669a025a313c966c10f3f12aca
Salomon Chambi <salomonchambi@gmail.com>
2024-03-05 09:00:00 -0500

    feat: return a string of all set values that start with a specific string

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -6,7 +6,12 @@ export default function cleanSet(set, startString) {
     return '';
   }
 
-  return [...set].filter((item) => typeof item === 'string' && item.startsWith(startString))
+  // Convert the Set 'set' into an array using the spread operator and filter it based on conditions
+  return [...set]
+    // Filter the array to include only items that are strings and start with the specified
+    // 'startString'
+    .filter((item) => typeof item === 'string' && item.startsWith(startString))
+    // For each filtered item, remove the 'startString' from the beginning using slice
     .map((item) => item.slice(startString.length))
     .join('-');
 }
