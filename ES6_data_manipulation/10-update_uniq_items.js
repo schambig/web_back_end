4d2f84505a8d2e3145cbad21871c82e3f6e03cc1
Salomon Chambi <salomonchambi@gmail.com>
2024-03-12 09:00:00 -0500

    feat: return an updated map for all items with initial qty at 1

--- ES6_data_manipulation/10-update_uniq_items.js
+++ ES6_data_manipulation/10-update_uniq_items.js
@@ -12,3 +12,21 @@ export default function updateUniqueItems(map) {
     }
   });
 }
+
+/* const updateUniqueItems = (map) => {
+  if (!(map instanceof Map)) {
+    throw new Error('Cannot process');
+  }
+
+  // Iterate over each key-value pair in the Map using for...of loop with destructuring
+  for (const [key, value] of map) {
+    // For each key-value pair, check if the value is equal to 1
+    if (value === 1) {
+      map.set(key, 100);
+    }
+  }
+
+  return map;
+};
+
+export default updateUniqueItems; */
