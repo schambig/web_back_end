8ed849401bc9fe715ed2480b9bafc507d5e82510
Salomon Chambi <salomonchambi@gmail.com>
2024-04-08 09:00:00 -0500

    feat: implement a class that accept an attribute

---
+++ ES6_classes/0-classroom.js
@@ -0,0 +1,6 @@
+export default class ClassRoom {
+  constructor(maxStudentsSize) {
+    // _ prefixed variable names are considered private by convention but are still public.
+    this._maxStudentsSize = maxStudentsSize;
+  }
+}
