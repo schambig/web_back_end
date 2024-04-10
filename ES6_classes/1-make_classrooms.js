d420c2398c0d57bd983e288c6ec716924f486f69
Salomon Chambi <salomonchambi@gmail.com>
2024-04-10 09:00:00 -0500

    feat: implement a function that uses a imported class and return an array

--- ES6_classes/1-make_classrooms.js
+++ ES6_classes/1-make_classrooms.js
@@ -1,9 +1,15 @@
 import ClassRoom from './0-classroom';
 
 export default function initializeRooms() {
+  return [new ClassRoom(19), new ClassRoom(20), new ClassRoom(34)];
+}
+
+/*
+export default function initializeRooms() {
   const room1 = new ClassRoom(19);
   const room2 = new ClassRoom(20);
   const room3 = new ClassRoom(34);
 
   return [room1, room2, room3];
 }
+ */
